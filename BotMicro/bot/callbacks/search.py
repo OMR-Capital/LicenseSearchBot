97b4b8a7706bd05cd985befa327f069f6451b425
butvinm <butvin.mihail@yandex.ru>
2023-07-04 23:08:15 +0300

    Remove search button: search on each message

--- BotMicro/bot/callbacks/search.py
+++
@@ -1,6 +0,0 @@
-from typing import Optional
-from aiogram.filters.callback_data import CallbackData
-
-
-class StartSearchCallback(CallbackData, prefix='search'):
-    previous_package_key: Optional[str]
