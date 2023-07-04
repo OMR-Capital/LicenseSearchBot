3a1c9cc9a4abf14629e68053679b67bfb4176307
butvinm <butvin.mihail@yandex.ru>
2023-07-04 23:06:45 +0300

    Add search limits

--- BotMicro/search_utils/direct_search.py
+++ BotMicro/search_utils/direct_search.py
@@ -1,14 +1,23 @@
+from typing import Optional
 from models import Item
 from search_utils.utils import normalize
 
 
-def direct_search(query: str, items: list[Item]) -> list[Item]:
+def direct_search(
+    query: str,
+    items: list[Item],
+    limit: Optional[int] = None,
+) -> list[Item]:
     query = normalize(query)
     if len(query) > 6:
         query = query[:-1]
 
-    relevant_items = [
-        item for item in items
-        if query in normalize(item.name)
-    ]
+    relevant_items = []
+    for item in items:
+        if query in normalize(item.name):
+            relevant_items.append(item)
+
+        if limit and len(relevant_items) >= limit:
+            break
+
     return relevant_items
--- BotMicro/search_utils/fuzzy_search.py
+++ BotMicro/search_utils/fuzzy_search.py
@@ -1,23 +1,27 @@
+from typing import Optional
 from fuzzywuzzy import fuzz
 
 from models import Item
 from search_utils.utils import normalize
 
 
-def fuzzy_search(query: str, items: list[Item]) -> list[Item]:
-    query = normalize(query)    
+def fuzzy_search(query: str, items: list[Item], limit: Optional[int] = None) -> list[Item]:
+    query = normalize(query)
     query_words = query.split()
     min_score = len(query_words) * 85
 
-    scored_items: list[tuple[Item, int]] = []   
+    scored_items: list[tuple[Item, int]] = []
     for item in items:
         score = 0
         item_name = normalize(item.name)
         for word in query_words:
-            score += fuzz.partial_ratio(word, item_name) # type: ignore
-        
+            score += fuzz.partial_ratio(word, item_name)  # type: ignore
+
         if score >= min_score:
             scored_items.append((item, score))
+
+        if limit and len(scored_items) >= limit:
+            break
 
     scored_items.sort(key=lambda pair: pair[1], reverse=True)
     relevant_items = [item for item, score in scored_items]
